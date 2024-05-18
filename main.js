53de20cc5a851f72ae94d589f401506f1e9e0abf
joshibhuwan1 <bhuwanjoc2006@gmail.com>
2024-05-18 11:46:12 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 document.getElementById("notesButton").addEventListener("click", function() {
-  window.location.href = "https://joshibhuwan1.github.io/Web";
+  window.location.href = "https://joshibhuwan1.github.io/web";
 })
